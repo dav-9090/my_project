55a5e917d87ed9c9b3579909c4d2195cc520eb2e
Anatoliy <antworl.it@gmail.com>
2020-06-30 14:32:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// alert("Hello"); 
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("вам есть 18?", "18");
+// console.log(answer + 5);
+
+
+const answers = [];
+answers[0] = prompt('Как вас зоваут?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); 
